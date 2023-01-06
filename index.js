da39bd2e6c6bb65adfa5a222cdc48a5d68ce2418
dosaev_raiska <frazycrazy1988@gmail.com>
2023-01-06 17:00:14 +0300

    fullFinalFrontP1

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 if ($(document).height() <= $(window).height())
   $("footer.footer").addClass("navbar-fixed-bottom");
+
+  function chpok(id){
+    elem = document.getElementById(id); //находим блок div по его id, который передали в функцию
+    state = elem.style.display; //смотрим, включен ли сейчас элемент
+    if (state =='') elem.style.display='none'; //если включен, то выключаем
+    else elem.style.display=''; //иначе - включаем
+}
